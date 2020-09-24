e4e57b3682cedff4eaae6eb226ad80663a8c86ce
juliaviolet <juliacarolineviolet@gmail.com>
2020-09-24 15:55:05 -0400

    Create Format_String.py

---
+++ Format_String.py
@@ -0,0 +1,5 @@
+name = "Manny"
+number = len(name) * 3
+print("Hello {}, your lucky number is {}".format(name,number))
+print("Your lucky number is {number}, {name}.".format(name=name, number=len(name)*3))
+print("{name} received {grade}% on the exam.".format(name,grade))
