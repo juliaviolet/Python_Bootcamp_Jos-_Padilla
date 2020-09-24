9ed8a7e36b831720b96df7da32d95ae3913df933
juliaviolet <juliacarolineviolet@gmail.com>
2020-09-24 15:27:16 -0400

    Create 2D_Numpy_Arrays.py

---
+++ 2D_Numpy_Arrays.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+np_2D = np.array([[1.73,1.68,1.71,1.89,1.79],[65.4,59.2,63.6,88.4,68.7]])
+
+print(np_2D)
+
+print(np_2D.shape)
+
+print(np_2D[0][2])
+
+print(np_2D[0,2])
+
+print(np_2D[:,1:3])
+
+print(np_2D[1,:])
