e671269c61bb968124cd7f5a9d94bbcdb47a32c1
juliaviolet <juliacarolineviolet@gmail.com>
2020-09-24 15:53:30 -0400

    Create Skip_Elements.py

---
+++ Skip_Elements.py
@@ -0,0 +1,8 @@
+def skip_elements(elements):
+	odd = elements[::2]
+
+	return odd
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements([])) # Should be []
